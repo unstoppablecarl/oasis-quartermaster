5dadb32f162b5c09c662c2b387fe8a0d1811e367
Carl Olsen <unstoppablecarlolsen@gmail.com>
2026-09-11 13:51:15 -0400

    initial

---
+++ database/migrations/2026_09_11_173828_add_lists_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lists', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class, '')->index();
+            $table->string('uuid')->unique()->index();
+            $table->string('display_name');
+            $table->timestamps('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lists');
+    }
+};
