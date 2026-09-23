3d0089f9f1669d5eaf838c3604adcc8c432df54e
Carl Olsen <unstoppablecarlolsen@gmail.com>
2026-09-23 13:17:07 -0400

    initial

---
+++ resources/js/pages/ArmyLists/Components/ButtonToggle.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { PhCheck, PhX } from '@phosphor-icons/vue'
+import { BButton } from 'bootstrap-vue-next'
+
+const value = defineModel<boolean>({ required: true })
+</script>
+<template>
+    <BButton
+        v-model:pressed="value"
+        :variant="value ? 'success' : 'danger'"
+    >
+        <PhCheck v-if="value" />
+        <PhX v-else />
+        &nbsp;
+        <slot/>
+    </BButton>
+</template>
